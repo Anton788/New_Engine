dbf69ca2151e82fa57b647fb32724417b7abc08a
boriskatru <53528032+boriskatru@users.noreply.github.com>
2019-08-22 22:11:43 +0300

    Add files via upload

--- Mags_and_coils.h
+++ Mags_and_coils.h
@@ -14,9 +14,47 @@ using namespace std;
 typedef struct Field_cell
 {
 	vecter Mag_vec;
+	vector<vecter> jacobian;
+	Field_cell() : jacobian(3) {
 
+	}
+	Field_cell operator +(Field_cell & cell) {
+		Field_cell tmp = *this;
+		tmp.Mag_vec += cell.Mag_vec;
+		for (unsigned int i = 0; i < 3; i++) {
+			tmp.jacobian[i] += cell.jacobian[i];
+		}
+		return tmp;
+	}
+	void show()
+	{
+		cout << Mag_vec.x_proj << "	" << Mag_vec.y_proj << "	" << Mag_vec.z_proj << endl;
+	}
+	
 } Field_cell;
 
+Field_cell approx(Field_cell first, Field_cell cells[4], double dx, double dy) {
+	Field_cell tmp = first;
+	tmp.Mag_vec += first.jacobian[0] * dx + first.jacobian[1] * dy;
+	for (int i = 0; i < 3; i++) {
+		if ((dx > 0) && (dy > 0)) {
+			tmp.jacobian[i] = (1 - dx) * first.jacobian[i] + dx * cells[0].jacobian[i];
+		}
+		if ((dx < 0) && (dy > 0)) {
+			tmp.jacobian[i] = (1 - dx) * first.jacobian[i] + dx * cells[1].jacobian[i];
+		}
+		if ((dx < 0) && (dy < 0)) {
+			tmp.jacobian[i] = (1 - dx) * first.jacobian[i] + dx * cells[2].jacobian[i];
+		}
+		if ((dx > 0) && (dy < 0)) {
+			tmp.jacobian[i] = (1 - dx) * first.jacobian[i] + dx * cells[3].jacobian[i];
+		}
+		
+	}
+	return tmp;
+	
+	
+}
 class Field_2D {
 public:
 	void *magnit;
@@ -40,6 +78,99 @@ public:
 	double Flow_X_flat(double distance, int diametr);
 };
 
+class Full_Field_Line {
+public:
+	vector<Field_cell> up;
+	vector<Field_cell> down;
+	Field_cell& operator [](int y) {
+		if (y > 0) {
+			return (up[y]);
+		}
+		else {
+			return (down[-y]);
+		}
+	}
+	/*vecter operator [](double y) {
+		if (y > 0) {
+			return (up[y].Mag_vec);
+		}
+		else {
+			return (down[-y].Mag_vec);
+		}
+	}*/
+	Full_Field_Line() {};
+	Full_Field_Line(int size_y) :up(size_y), down(size_y) {}
+};
+
+class Full_Field {
+public:
+	vecter center_coord;
+
+	int size_x, size_y;
+
+	vector<Full_Field_Line> left;
+	vector<Full_Field_Line> right;
+
+	Full_Field_Line& operator [] (int x)
+	{
+		if (x >= 0) {
+			return (right[x]);
+		}
+		else {
+			return (left[-x]);
+		}
+	};
+	Full_Field() {};
+	Full_Field(Electromagnet magnet) :
+		size_x(magnet.Mag_field.size_x),
+		size_y(magnet.Mag_field.size_y),
+		center_coord(magnet.position),
+		left(magnet.Mag_field.size_x, Full_Field_Line(size_y)),
+		right(magnet.Mag_field.size_x, Full_Field_Line(size_y)) {
+		for (int x = 0; x++; x < magnet.Mag_field.size_x) {
+			for (int y = 0; y++; y < magnet.Mag_field.size_y) {
+				right[x].up[y].Mag_vec = magnet.Mag_field.cells[x][y].Mag_vec;
+
+				right[x].down[y].Mag_vec = magnet.Mag_field.cells[x][y].Mag_vec;
+				right[x].down[y].Mag_vec.y_proj = -magnet.Mag_field.cells[x][y].Mag_vec.y_proj;
+
+				left[x].down[y].Mag_vec = magnet.Mag_field.cells[x][y].Mag_vec;
+
+				left[x].up[y].Mag_vec = -right[x].down[y].Mag_vec;
+			}
+		}
+	}
+	Full_Field(int size_x, int size_y) :
+		size_x(size_x),
+		size_y(size_y),
+		left(size_x, size_y),
+		right(size_x, size_y)
+	{
+		for (int i = 0; i < size_x; i++) {
+			for (int k = 0; k < size_y; k++) {
+				right[i].up[k].Mag_vec.set(1, 1, 0);
+				left[i].up[k].Mag_vec.set(-1, 1, 0);
+				left[i].down[k].Mag_vec.set(-1, -1, 0);
+				right[i].down[k].Mag_vec.set(1, -1, 0);
+			}
+		};
+	};
+	Full_Field &operator + (Full_Field Field2) {
+		int tmp_x, tmp_y;
+		tmp_x = (size_x + Field2.size_x + abs(center_coord.x_proj - Field2.center_coord.x_proj)) / 2;
+		tmp_y = (size_y + Field2.size_y + abs(center_coord.y_proj - Field2.center_coord.y_proj)) / 2;
+		Full_Field tmp(tmp_x, tmp_y);
+		tmp.center_coord.x_proj = min(center_coord.x_proj + size_x, Field2.center_coord.x_proj + Field2.size_x) - tmp_x;
+		tmp.center_coord.y_proj = min(center_coord.y_proj + size_y, Field2.center_coord.y_proj + Field2.size_y) - tmp_y;
+		for (int _x = -tmp_x + 1; _x < tmp_x; _x++) {
+			for (int _y = -tmp_y + 1; _y < tmp_y; _y++) {
+				tmp[_x][_y] = (*this)[ -center_coord.x_proj + tmp.center_coord.x_proj + _x][-center_coord.y_proj + tmp.center_coord.y_proj + _y] +
+					Field2[-Field2.center_coord.x_proj + tmp.center_coord.x_proj + _x][-Field2.center_coord.y_proj + tmp.center_coord.y_proj + _y];
+			}
+		}//APROX SUKAAAAAAA!HHH!UG!GY!TYGTY!GU!U!UY!G!G!GG!UG!UGIY!GIYT!TYU !DYTCdrRYDY R@RD !@D Y Yut @# @#H VV@ T YDCV  #uV#H#$gh
+
+	}
+};
 
 class Electromagnet {
 
@@ -88,6 +219,8 @@ public:
 	Magnet(int destiny_of_mag_pixels, double _strength, double _diametr, double _weight,
 		double _height, vecter _position, vecter _speed, int _sight);
 	 
+	void show_field(int cherez_n);
+
 	void relocate(double dtime);
 
 	void Set_Field2D_Conf(int prec_H, int prec_R, int _size_x, int _size_y);
